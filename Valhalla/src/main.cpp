cfa057b218f04e1968c5d124bf625f7d3e1de5d2
Brian Campuzano <brian.campuzano@colorado.edu>
2013-12-06 12:43:02 -0700

    Added switch statement, played with uint64 values, need to test with a trace file

--- Valhalla/src/main.cpp
+++ Valhalla/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 #include "StdTypes.h"
 #include "MemoryModule.h"
@@ -15,9 +16,7 @@ int main(int argc, char ** argv)
   uint64 address;
   uint32 bytesize;
   uint64 time;
-  CacheOperation operation;
   
-
   cout << "Creating Main Memory." << endl;
   MemoryModule * mainMemory = new MemoryModule();
   mainMemory->printMemoryModuleSetup();
@@ -36,8 +35,7 @@ int main(int argc, char ** argv)
   cout << "Creating L1 Data Cache." << endl;
   MemoryModule * l1DataCache = new MemoryModule(L1_BLOCK_SIZE,
                                                 L1_MEMORY_SIZE,
-                                                //L1_ASSOCIATIVITY,
-                                                2,
+                                                L1_ASSOCIATIVITY,
                                                 L1_HIT_PENALTY,
                                                 L1_MISS_PENALTY,
                                                 L2_TRANSFER_TIME,
@@ -55,32 +53,44 @@ int main(int argc, char ** argv)
                                                 L2_TRANSFER_TIME,
                                                 L2_TRANSFER_WIDTH,
                                                 l2Cache);
+ 
+  while (scanf("%c %luox %luox",&op,&address,&bytesize) == 3)
+    { 
+      switch(op)
+        {
+        case 'I':
+          //Intruction fetch
+          time = l1InstCache->checkMemoryEntry(CACHE_READ, address, bytesize);
+          break;
+        case 'R':
+          time = l1DataCache->checkMemoryEntry(CACHE_READ, address, bytesize);
+          break;
+        case 'W':
+          time = l1DataCache->checkMemoryEntry(CACHE_WRITE, address, bytesize);
+          break;
+        default:
+          continue;
+        }
+      cout << "Address to lookup: " << address << endl;
+      cout << "Time for memory lookup: " << time << endl;
+    }
+  /*
+    uint64 time = l1DataCache->checkMemoryEntry(CACHE_WRITE, 65536, 32);
+    cout << "Time for memory lookup 1: " << time << endl;
+    time = l1DataCache->checkMemoryEntry(CACHE_WRITE, 4096, 32);
+    cout << "Time for memory lookup 2: " << time << endl;
 
-while (scanf(”%c %Lx %dnn”,&op,&address,&bytesize) == 3){ 
-    if(!"I".compare(op)) {
-    operation = CACHE_WRITE;
-    } else operation = CACHE_READ;
-    time = l1DataCache->checkMemoryEntry(operation, address, bytesize);
-    cout << "Time for memory lookup: " << time << endl;
-}
+    time = l1DataCache->checkMemoryEntry(CACHE_READ, 8192, 32);
+    cout << "Time for memory lookup 3: " << time << endl;
+    time = l1DataCache->checkMemoryEntry(CACHE_READ, 256, 32);
+    cout << "Time for memory lookup 4: " << time << endl;
 
-/*
-  uint64 time = l1DataCache->checkMemoryEntry(CACHE_WRITE, 65536, 32);
-  cout << "Time for memory lookup 1: " << time << endl;
-  time = l1DataCache->checkMemoryEntry(CACHE_WRITE, 4096, 32);
-  cout << "Time for memory lookup 2: " << time << endl;
-
-  time = l1DataCache->checkMemoryEntry(CACHE_READ, 8192, 32);
-  cout << "Time for memory lookup 3: " << time << endl;
-  time = l1DataCache->checkMemoryEntry(CACHE_READ, 256, 32);
-  cout << "Time for memory lookup 4: " << time << endl;
-
-  time = l1DataCache->checkMemoryEntry(CACHE_READ, 512, 32);
-  cout << "Time for memory lookup 5: " << time << endl;
+    time = l1DataCache->checkMemoryEntry(CACHE_READ, 512, 32);
+    cout << "Time for memory lookup 5: " << time << endl;
   
-  l1DataCache->printMemoryEntries();
-  l2Cache->printMemoryEntries();
-*/
+    l1DataCache->printMemoryEntries();
+    l2Cache->printMemoryEntries();
+  */
   
   cout << "Test Complete." << endl;
   return 0;
